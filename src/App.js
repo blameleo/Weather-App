1baf99220af126268fc8dc269e9ca7da492c5062
blameleo <leonardgre@gmail.com>
2023-02-16 16:26:14 +0000

    added error handling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Display from "./components/Display";
 import Inputside from "./components/Inputside";
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 //create state for input and sotre it
 //create state in top level app and pass as props
@@ -11,31 +11,59 @@ import axios from "axios";
 
 export default function App() {
   const [state, setState] = useState("");
+  const [err, setErr] = useState(null);
+  const [isloading, setIsLoading] = useState(null);
 
   function handleSearch(inputText) {
     // setState(inputText);
+    setIsLoading(true);
     const url = `https://api.weatherapi.com/v1/current.json?key=ea75c4ec67f449458ec120109230902&q=${inputText}`;
-    axios.get(url).then((res) => {
-      setState(res.data);
-    });
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("city not found");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setState(data);
+        setIsLoading(false);
+        setErr(false)
+      })
+      .catch((err) => {
+
+        setErr(err.message);
+        setIsLoading(false);
+
+      });
   }
 
   return (
-    <div className="sm:grid grid-cols-3 h-screen back ">
-      <Display
-        icon={state.current?.condition.icon}
-        condition={state.current?.condition.text}
-        degree={state.current?.temp_c}
-        name={state.location?.name}
-        time={state.location?.localtime}
-      />
-      <Inputside
-        click={handleSearch}
-        cloudy={state.current?.cloud}
-        humidity={state.current?.humidity}
-        wind={state.current?.wind_mph}
-        feels={state.current?.feelslike_c}
-      />
+    <div>
+      {err && (
+        <p className="text-center bg-red-500 text-white">
+          please enter a valid city or country
+        </p>
+      )}
+
+      {isloading && <p className="text-center bg-red-500 text-white">loading</p>}
+      <div className="sm:grid grid-cols-3 h-screen back ">
+        <Display
+          icon={state.current?.condition.icon}
+          condition={state.current?.condition.text}
+          degree={state.current?.temp_c}
+          name={state.location?.name}
+          time={state.location?.localtime}
+        ></Display>
+        <Inputside
+          click={handleSearch}
+          cloudy={state.current?.cloud}
+          humidity={state.current?.humidity}
+          wind={state.current?.wind_mph}
+          feels={state.current?.feelslike_c}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,12 @@
-
+// import { useState } from "react";
 
 const Display = (props) => {
+
   return (
     <div className=" grid pl-10  sm:h-screen col-span-2  text-white">
       <h1 className="mb-10 sm:mb-0 mt-10 font-black">The Weather App</h1>
 
-      <div className="sm:flex sm:justify-around items-center   ">
+    <div className="sm:flex sm:justify-around items-center   ">
         {props.degree ? <h1 className="text-8xl">{props.degree}°C</h1> : null}
         <div className="py-10 sm:py-0">
           <h1 className="text-4xl font-extrabold">{props.name}</h1>
@@ -13,11 +14,9 @@ const Display = (props) => {
         </div>
         <div className="mb-5 sm:mb-0">
           <img src={props.icon} alt="" />
-          <p >{props.condition}</p>
+          <p>{props.condition}</p>
         </div>
       </div>
-
-  
     </div>
   );
 };
